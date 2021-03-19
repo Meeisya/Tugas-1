72ed199fbb7004155920b5e21f5c7823a741b4a8
Meeisya <Meeisya55@gmail.com>
2021-03-19 16:09:00 +0700

    Soal-2

--- Soal-2.py
+++ Soal-2.py
@@ -2,5 +2,5 @@ print("Aplikasi Luas Lingkaran");
 print("=======================");
 jari=input("Masukkan Nilai Jari-jari Lingkaran :");
 luas_lingkaran = 22 * int(jari) * int(jari)/7
-print("Luas lingkaran dengan jari-jari ",jari, "cm adalah "'%.2f'%luas_lingkaran, "cm\uOOb2.");
+print("Luas lingkaran dengan jari-jari ",jari, "cm adalah "'%.2f'%luas_lingkaran, "cm\u00b2.");
 
